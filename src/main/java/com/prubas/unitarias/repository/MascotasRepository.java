61efbc3d1123e00a1aa389049b9f59e5b5b9e456
Chiispyn <dem.monsalves@duocuc.cl>
2025-06-10 12:54:13 -0400

    test1.1

--- src/main/java/com/prubas/unitarias/repository/MascotasRepository.java
+++ src/main/java/com/prubas/unitarias/repository/MascotasRepository.java
@@ -2,6 +2,9 @@ package com.prubas.unitarias.repository;
 
 import com.prubas.unitarias.model.Mascota;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+
+@Repository
 public interface MascotasRepository extends JpaRepository<Mascota, Long>{}
--- src/test/java/com/prubas/unitarias/service/MascotaServiceTest.java
+++ src/test/java/com/prubas/unitarias/service/MascotaServiceTest.java
@@ -1,5 +1,60 @@
 package com.prubas.unitarias.service;
 
-public class MascotaServiceTest {
+import com.prubas.unitarias.model.Mascota;
+import com.prubas.unitarias.repository.MascotasRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MascotaServiceTest {
+    @Mock
+    private MascotasRepository mascotasRepository;
+
+    @InjectMocks
+    private MascotaService mascotaService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    /*para guardar mascota en la capa servicio */
+    @Test
+    void testGuardarMascota(){ 
+    Mascota mascota = new Mascota(null, "Rex", "Perro", 5);
+    Mascota mascotaGuardada = new Mascota(1L, "Rex", "Perro", 5);
+    
+    when(mascotasRepository.save(mascota)).thenReturn(mascotaGuardada);
+
+    Mascota resultado = mascotaService.guardarMascota(mascota);
+    
+    assertThat(resultado.getId()).isEqualTo(1L);
+    assertThat(resultado.getNombre()).isEqualTo("Rex");
+    assertThat(resultado.getTipo()).isEqualTo("Perro");
+    assertThat(resultado.getEdad()).isEqualTo(5);
+    
+    verify(mascotasRepository).save(mascota);
+}
+    @Test
+    void testListarMascotas() {
+        Mascota m1 = new Mascota(1L, "Rex", "Perro", 5);
+        Mascota m2 = new Mascota(2L, "Michi", "Gato", 2);
+        when(mascotasRepository.findAll()).thenReturn(Arrays.asList(m1, m2));
+
+        List<Mascota> resultado = mascotaService.listarMascotas();
+        assertThat(resultado).hasSize(2).contains(m1, m2);
+        verify(mascotasRepository).findAll();
+    }   
 
 }
